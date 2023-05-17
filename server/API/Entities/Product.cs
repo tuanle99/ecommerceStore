67b7c23092c139e511d4939baec2aaca6416cf59
tuanhoangle <tuanleprofessional@gmail.com>
2023-05-17 15:28:00 -0400

    api controller

--- server/API/Entities/Product.cs
+++ server/API/Entities/Product.cs
@@ -1,3 +1,15 @@
+/* Entity Framework features
+Modelling
+Querying
+Change Tracking
+Saving
+Concurrency
+Transationcs
+Caching - store a subset of data that can be retrive faster than calling from the databases
+Built-in conventions
+Configurations
+*/
+
 namespace API.Entities
 {
     public class Product
@@ -7,6 +19,7 @@ namespace API.Entities
         public string Description { get; set; }
         public long Price { get; set; }
         public string PictureUrl { get; set; }
+        public string Brand { get; set; }
         public string Type { get; set; }
         public int QuantityInStock { get; set; }
     }
